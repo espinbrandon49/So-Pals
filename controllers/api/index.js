95d51340dc7a908c40fc88314e9e14ee947d6758
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-15 23:36:17 -0700

    do user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const connection = require('../../config/connection');
 
 router.use('/users', userRoutes);
 
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const User = require('../../models/User');
-const connection = require('../../config/connection')
 
-router.get('/all-users', (req, res) => {
-  // Using model in route to find all documents that are instances of that model
+router.get('/all-user', (req, res) => {
   User.find({}, (err, result) => {
     if (result) {
       res.status(200).json(result);
@@ -12,6 +10,29 @@ router.get('/all-users', (req, res) => {
       res.status(500).json({ message: 'something went wrong' });
     }
   });
+});
+
+router.get('/one-user/:id', (req, res) => {
+  User.findOne({ _id: req.params.id }, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
+  });
+});
+
+router.post('/new-user', (req, res) => {
+  console.log(req.body)
+  const newUser = new User(req.body);
+  newUser.save();
+  if (newUser) {
+    res.status(200).json(newUser);
+  } else {
+    console.log('Uh Oh, something went wrong');
+    res.status(500).json({ message: 'something went wrong' });
+  }
 });
 
 module.exports = router;
