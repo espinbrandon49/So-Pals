3c47293af3d4518ee00764eed4afd9f6f2cbbb89
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-16 10:02:30 -0700

    Thought put route

--- controllers/api/thoughtRoutes.js
+++ controllers/api/thoughtRoutes.js
@@ -36,6 +36,28 @@ router.post('/new-thought', (req, res) => {
   }
 });
 
+// Finds the first document with the name with the value equal to 'Kids' and updates that name to the provided URL param value
+router.put('/update-thought/:id', (req, res) => {
+  // Uses findOneAndUpdate() method on model
+  Thought.findOneAndUpdate(
+    // Finds first document with name of "Kids"
+    { _id: req.params.id },
+    // Replaces name with value in URL param
+    { thoughtText: req.body.thoughtText ? req.body.thoughtText : Thought.thoughtText },
+    // Sets to true so updated document is returned; Otherwise original document will be returned
+    { new: true },
+    (err, result) => {
+      if (result) {
+        res.status(200).json(result);
+        console.log(`Updated: ${result}`);
+      } else {
+        console.log('Uh Oh, something went wrong');
+        res.status(500).json({ message: 'something went wrong' });
+      }
+    }
+  );
+});
+
 // Finds first document that matches and deletes
 router.delete('/delete-thought/:id', (req, res) => {
   Thought.findOneAndDelete({ name: req.params.id }, (err, result) => {
